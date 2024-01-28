2bd20c3225e297647393a82f567d4fd697b792d9
souleymane <soultaka4229@gmail.com>
2024-01-28 16:46:31 +0000

    mise en forme du navbar avec les infos de l'utilisateur

--- src/app/pages/dashboard/components/navbar/navbar.component.ts
+++ src/app/pages/dashboard/components/navbar/navbar.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatIconModule,MatButtonModule,MatToolbarModule],
+  imports: [MatIconModule, MatButtonModule, MatToolbarModule, MatMenuModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
