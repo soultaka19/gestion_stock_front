1fde1ccb5e483cca1df5891078ec03a372ad75c3
Souleymane Diallo <soultaka4229@gmail.com>
2024-05-25 18:43:08 +0000

    qwert

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { provideAnimations, provideNoopAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideNoopAnimations(),provideHttpClient(withFetch())]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideNoopAnimations(),
+    provideHttpClient(withFetch()),
+    provideAnimations(),
+  ],
 };
